e15580752aac1cf173b6bfeec1aa7f788c960f77
Jardel Lima <ti.jardel@grpmeireles.net>
2018-10-24 11:06:50 -0300

    Fundamentos Básicos

---
+++ fundamentosBasicos01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Fundamentos Básicos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>-->
+</head>
+<body>
+    <h1>Olá mundo!</h1>
+
+    <?php
+    echo "Meu nome é Jubileu.<br>";
+    print "Minha idade é 20 anos.";
+    //Esse é um comentário
+    //First code php
+    
+
+    ?>
+</body>
+</html>
---
+++ pontuacao.php
@@ -0,0 +1,8 @@
+<?php
+
+$nome = 'Rodrigo Oliveiro'
+echo 'Meu nome é $nome';//Aspas simples são literais, não interpreta
+echo 'Meu nome é '.$nome;//Jeito correto 
+echo"<hr>"
+
+echo "Meu nome é $nome"; //Aspas duplas são interpretativas.
