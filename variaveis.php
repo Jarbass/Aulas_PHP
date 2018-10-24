c186754d5d0b26b57b58cf43a4882de99b319330
Jardel Lima <ti.jardel@grpmeireles.net>
2018-10-24 11:02:43 -0300

    Variáveis em PHP

---
+++ variaveis.php
@@ -0,0 +1,20 @@
+<?php
+//Meus dados
+$nome = "Jubileu da Silva";
+$idade = 20;
+$altura = 1.81;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+//Alterando meus dados
+$nome = "Doidim";
+$idade = 29;
+$altura = 1.75;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
+echo "<hr>";
+
+//php is case sensitive
+$carro = "Hilux";
+
+echo $carro;
---
+++ variaveisDinamicas.php
@@ -0,0 +1,14 @@
+<?php
+
+$bebida = "Refrigerante";
+
+
+$$bebida = "Guaraná";
+echo $Refrigerante;
+echo "<br>";
+
+$destino = "Cidade";
+/*Atribuindo assim o valor destino é === Cidade*/
+$$destino = "Fortaleza";
+/*Sendo assim, echo "$Cidade" = "Fortaleza*/
+echo $Cidade;
